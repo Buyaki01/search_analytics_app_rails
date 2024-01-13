ab7a9d48d144ccfb02b3275301e0d77eb2f06658
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-13 15:41:01 +0300

    Add SearchesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'searches#index'
+
+  get 'searches/index'
+  post 'searches/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
 end
