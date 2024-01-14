f58d38f9208e5f8f9875ee4f3589308bc7d43e32
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-14 07:07:24 +0300

    Add iterate metrhod to iterate through the 20 items from wikipedia

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,38 +1,60 @@
 class SearchesController < ApplicationController
   require 'net/http'
   require 'json'
-
-  def index
-  end
+  include ActionView::Helpers::SanitizeHelper
 
   def create
     search_query = params[:search_query]
 
-    user_ip = request.remote_ip
+    if search_query.present?
+      user_ip = request.remote_ip
   
-    wikipedia_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=#{search_query}&utf8=1&srlimit=20&prop=extracts|pageimages&exintro=true&piprop=thumbnail&pithumbsize=300&titles=#{search_query}&utf8=1&srlimit=20"
+      wikipedia_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=#{search_query}&utf8=1&srlimit=20&prop=extracts|pageimages&exintro=true&piprop=thumbnail&pithumbsize=300&titles=#{search_query}&utf8=1&srlimit=20"
 
-    begin
-      uri = URI(wikipedia_url)
-      response = Net::HTTP.get(uri)
-      data = JSON.parse(response)
+      begin
+        uri = URI(wikipedia_url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
 
-      if data['query'] && data['query']['pages']
-        # Extract relevant information
-        page_info = data['query']['pages'].values.first
-        pageid = page_info['pageid']
-        title = page_info['title']
-        text = page_info['extract']
-        image_url = page_info.dig('thumbnail', 'source')
+        if data['query'] && data['query']['search']
+          search_results = data['query']['search']
+          
+          # Create an array to store the results
+          @results_array = []
 
-      else
-        # Handle the case where 'pages' key is not present
-        Rails.logger.error("No pages found in the API response.")
+          search_results.each do |result|
+            pageid = result['pageid']
+            title = result['title']
+
+            # Use the full_sanitizer to remove HTML tags from the text
+            text_with_tags = result['snippet']
+            text_without_tags = strip_tags(text_with_tags)
+
+            # Create a hash with relevant information and add it to the array
+            result_hash = {
+              pageid: pageid,
+              title: title,
+              text: text_without_tags,
+            }
+
+            @results_array << result_hash
+          end
+
+        else
+          # Handle the case where 'pages' key is not present
+          Rails.logger.error("No pages found in the API response.")
+          @results_array = []
+        end
+      rescue Errno::ETIMEDOUT => e
+        Rails.logger.error("Failed to connect to Wikipedia API: #{e.message}")
+        @results_array = []
+      rescue StandardError => e
+        Rails.logger.error("An error occurred: #{e.message}")
+        @results_array = []
       end
-    rescue Errno::ETIMEDOUT => e
-      Rails.logger.error("Failed to connect to Wikipedia API: #{e.message}")
-    rescue StandardError => e
-      Rails.logger.error("An error occurred: #{e.message}")
+    else
+       # If search_query is not present, don't perform a search
+       @results_array = []
     end
   end
 end
