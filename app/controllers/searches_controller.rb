9d631cb30720a5f62bd25ee7d82d26354e92bbd1
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-14 07:57:48 +0300

    Add most searched today method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,19 @@ class SearchesController < ApplicationController
   require 'json'
   include ActionView::Helpers::SanitizeHelper
 
+  before_action :most_searched_today, only: [:create]
+
   def create
     search_query = params[:search_query]
 
     if search_query.present?
       user_ip = request.remote_ip
+
+      Search.create!(
+        query: search_query,
+        user_ip: user_ip,
+        timestamp: Time.current
+      )
   
       wikipedia_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=#{search_query}&utf8=1&srlimit=20&prop=extracts|pageimages&exintro=true&piprop=thumbnail&pithumbsize=300&titles=#{search_query}&utf8=1&srlimit=20"
 
@@ -19,18 +27,15 @@ class SearchesController < ApplicationController
         if data['query'] && data['query']['search']
           search_results = data['query']['search']
           
-          # Create an array to store the results
           @results_array = []
 
           search_results.each do |result|
             pageid = result['pageid']
             title = result['title']
 
-            # Use the full_sanitizer to remove HTML tags from the text
             text_with_tags = result['snippet']
             text_without_tags = strip_tags(text_with_tags)
 
-            # Create a hash with relevant information and add it to the array
             result_hash = {
               pageid: pageid,
               title: title,
@@ -41,7 +46,6 @@ class SearchesController < ApplicationController
           end
 
         else
-          # Handle the case where 'pages' key is not present
           Rails.logger.error("No pages found in the API response.")
           @results_array = []
         end
@@ -53,8 +57,18 @@ class SearchesController < ApplicationController
         @results_array = []
       end
     else
-       # If search_query is not present, don't perform a search
        @results_array = []
     end
+  end
+
+  private
+
+  def most_searched_today
+    @most_searched_today = Search.where('created_at >= ?', Time.current.beginning_of_day)
+                                  .group(:query)
+                                  .order('count_query DESC')
+                                  .limit(10)
+                                  .count(:query)
+                                  .keys
   end
 end
